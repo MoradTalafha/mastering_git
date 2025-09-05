174ec323b416164a822cf9c92c9d437e47f8b250
MoradTalafha <moradtalafha2005@gmail.com>
2025-09-06 00:19:09 +0300

    work the process

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args){
         Calculator calculator = new Calculator();
         System.out.println("Hello, GitHub!");
-        //save the day
+
+        //in a process
+        //code
+        //code
+
     }
 }
