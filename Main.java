70d6e8b3b216b344fec7d9677c23abecc75b1a9d
MoradTalafha <moradtalafha2005@gmail.com>
2025-09-06 00:31:41 +0300

    as

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args){
         Calculator calculator = new Calculator();
+        System.out.println("Hello, GitHub!");
+
+        //in a process
+        //code
+        //codegit 
+
     }
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
-Hello, Git!
-
+##Hello, Git!
 Yo, johnny here!
 This is coming from 'div-adrian'
 I'm adding a calculator GUI feature!
